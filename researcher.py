2ae7c3c90fa83dd5d377b0eb30116012768a0f86
Serj57 <serjgus57@gmail.com>
2022-11-17 12:47:50 +0300

    Добавлен форматтер справки для отображения дефолтных значений

--- researcher.py
+++ researcher.py
@@ -31,19 +31,19 @@ if __name__ == '__main__':
     subparser = base_parser.add_subparsers()
 
     ### analyze subparser ###
-    analyze_parser = subparser.add_parser('analyze', help='Параметры анализа pcap(ng) файлов.')
+    analyze_parser = subparser.add_parser('analyze', help='Параметры анализа pcap(ng) файлов.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    analyze_parser.add_argument('-m', '--method', required=True, choices=['pos', 'pkt'], metavar='',
-                            help='Метод анализа (pos или pkt).')
+    analyze_parser.add_argument('-m', '--method', required=True, choices=['pos', 'pkt'],  metavar='',
+                            help='Метод анализа. Допустимые выражения: pos, pkt.')
 
     analyze_parser.add_argument('-r', '--range', type=int, default=32, metavar='',
-                            help='Число исследуемых позиций (default 32).')
+                            help='Число исследуемых позиций.')
 
     analyze_parser.add_argument('-l', '--length', type=int, default=10, metavar='',
-                            help='Число отображаемых байт на позицию (default 10).')
+                            help='Число отображаемых байт на позицию.')
     
     analyze_parser.add_argument('-c', '--count', type=int, default=5, metavar='',
-                            help='Число пакетов (default 5).')
+                            help='Число пакетов.')
 
     analyze_parser.set_defaults(handler=analyze_handler)
 
@@ -51,14 +51,14 @@ if __name__ == '__main__':
     file_parser = subparser.add_parser('pcap', help='Параметры обработки pcap(ng) файлов.')
     
     file_parser.add_argument('-p', '--proto', required=True, choices=['udp', 'tcp'], metavar='',
-                            help='Протокол транспортного уровня.')
+                            help='Протокол транспортного уровня. Допустимые выражения: udp, tcp.')
 
     file_parser.add_argument('-f', '--file', required=True, type=str, metavar='',
                             help='Файл дампа pcap(ng).')
 
     file_parser.set_defaults(handler=pcap_handler)
-
-  
+    
+    
     arg = base_parser.parse_known_args()
     arg[0].handler(arg[0])
 
